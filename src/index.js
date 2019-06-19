a5a7b4fb7720a546a04fdb1306279ab34bd1299f
Matheus Vieira <matheusvieiradepaula@gmail.com>
2019-06-19 15:38:50 -0300

    Config ENV variables

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-mongoose.connect('mongodb+srv://dbUser:dbUserPass2019@cluster0-cqz2q.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGOOSE_DATA, {
   useNewUrlParser: true,
 });
 
